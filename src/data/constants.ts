23a89b36d82e047ef741004d4f666ad779ab8f73
tyang019 <yang.tortrong@yahoo.com>
2026-03-23 11:00:46 -0700

    Create src/data/constants.ts with application constants

---
+++ src/data/constants.ts
@@ -0,0 +1,8 @@
+// Application Constants
+
+const APP_NAME = 'My Application';
+const APP_VERSION = '1.0.0';
+const API_ENDPOINT = 'https://api.example.com';
+const DEFAULT_TIMEOUT = 5000; // in milliseconds
+
+export { APP_NAME, APP_VERSION, API_ENDPOINT, DEFAULT_TIMEOUT };
