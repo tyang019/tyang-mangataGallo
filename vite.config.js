e58e175a2c5131cfea3ff9b9d2d41ab9634460af
tyang019 <tyang019@fiu.edu>
2026-03-26 20:29:39 -0700

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ensures relative paths for JS/CSS/assets
+  base: './', // relative paths for JS/CSS/assets
 });
